8898280b1e7232541af312036c14b66334ee944f
jiang <jiang@ipride.co.jp>
2020-10-15 11:33:54 +0900

    lint error

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -2,13 +2,14 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/hello")
 public class HelloController {
 
-    @RequestMapping("/init")
+    @GetMapping("/init")
     public String init(Model model) {
     	model.addAttribute("message", "Hello Thymeleaf!!");
         return "hello";
